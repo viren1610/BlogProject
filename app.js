a92522d4bcccef54c0f1e4b8ebdcaf829bae7f21
viren1610 <virenplays@gmail.com>
2023-01-23 15:02:39 +0530

    some dependecies

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express =require('express');
 const app = express();
 
+app.set('view engine','ejs')
+
 app.listen(3000);
 
 app.get('/',(req,res)=>{
@@ -17,5 +19,5 @@ app.get('/about-us',(req,res)=>{
 })
 
 app.use((req,res)=>{
-res.sendFile('./views/404.html',{root:__dirname})
+res.status(404).sendFile('./views/404.html',{root:__dirname})
 })
