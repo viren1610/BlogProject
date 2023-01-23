a515963f7ffa6731d3575367d7e6201536acf969
viren1610 <virenplays@gmail.com>
2023-01-23 14:12:37 +0530

    new project;

---
+++ app.js
@@ -0,0 +1,18 @@
+const express =require('express')
+const app = express();
+
+app.listen(3000);
+
+app.get('/',(req,res)=>{
+    res.send('<p>Homepage</p>')
+
+});
+app.get('/about',(req,res)=>{
+    res.send('<p>Aboutpage</p>')
+
+});
+app.get('/',(req,res)=>{
+    res.send('<p>Homepage</p>')
+
+});
+
